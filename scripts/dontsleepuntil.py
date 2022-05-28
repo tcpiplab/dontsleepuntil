6137d63f6bf4efaab3e21277966761d0e7fa3b7a
username-not-available-is-not-available <95994962+username-not-available-is-not-available@users.noreply.github.com>
2022-05-27 22:44:43 -0400

    Update dontsleepuntil.py
    better comments

--- scripts/dontsleepuntil.py
+++ scripts/dontsleepuntil.py
@@ -1,41 +1,44 @@
 #!/usr/bin/env python3
 
-def get_command():
+def get_command(): # gets the command to pass the arguments to the rest of the script
     import sys
     return sys.argv[1:]
 
 def dont_sleep_for_x_time(unit, time):
     from os import system
+    # multiplies the input based in the selected unit of time
+    # caffeintaes system for that amount of time in seconds
+    # sleeps after caffeinate finishes 
 
     if unit == '-s':
-        system(f'caffeinate -d -t {str(time)}')
+        system(f'caffeinate -d -t {str(time)}') # no change
         system('pmset displaysleepnow')
 
     elif unit == '-m':
-        system(f'caffeinate -d -t {str(int(time) * 60)}')
+        system(f'caffeinate -d -t {str(int(time) * 60)}') # 60 seconds in a day
         system('pmset displaysleepnow')
 
     elif unit == '-h':
-        system(f'caffeinate -d -t {str(int(time) * 3600)}')
+        system(f'caffeinate -d -t {str(int(time) * 3600)}') # 3600 seconds in a day
         system('pmset displaysleepnow')
     
     elif unit == '-d':
-        system(f'caffeinate -d -t {str(int(time) * 86400)}')
+        system(f'caffeinate -d -t {str(int(time) * 86400)}') # 86400 seconds in a day
         system('pmset displaysleepnow')
 
     else:
         print('Invalid unit. Please use -s, -m,-d, or -h.')
         
-def dont_sleep_for_x_batt(percent):
+def dont_sleep_for_x_batt(percent): # waits until battery reaches a percent to sleep
     import subprocess
     from os import system
     from time import sleep
-    while True:
-        battery_percent = str(subprocess.check_output(['pmset', '-g', 'batt']))
+    while True:     # while loop to constantly check the battery percent
+        battery_percent = str(subprocess.check_output(['pmset', '-g', 'batt'])) # isolates the battery percent in this string
         current_percent = int(battery_percent[battery_percent.index(')\\t') + 3:battery_percent.index('%')])
         if current_percent == int(percent):
-            break
-        system('caffeinate -d -t 1')
+            break # breaks if current battery reaches target percentage
+        system('caffeinate -d -t 1') # makes sure the system does not sleep
         sleep(1)
 
     system('pmset displaysleepnow')
@@ -43,20 +46,20 @@ def dont_sleep_for_x_batt(percent):
     
 def dont_sleep_until_command_finishes(command):
     from os import system
-    system(f'caffeinate -s {command}')
+    system(f'caffeinate -s {command}') # formats command into caffeinate command
     system('pmset displaysleepnow')
     
-def sleep_now():
+def sleep_now(): # sleeps immediately  
     from os import system
     system('pmset displaysleepnow')
     
 def never_sleep(voided=True):
     from os import system
-    system('caffeinate -i')
+    system('caffeinate -i') # never sleeps
     if not voided:
-        system('pmset displaysleepnow')
+        system('pmset displaysleepnow') # when the user exits with ctrl c, the computer wont sleep if its voided with -v
 
-def help_page():
+def help_page(): # help page that waits .05 seconds between each output so there is a rolling effect
     from time import sleep
     print("\n\nhelp page")
     sleep(.05)
@@ -73,10 +76,10 @@ def help_page():
     print("\n-h = help page: usage - dontsleepuntil -h (<-- prints this page)")
     print('\n')
 
-def interpret(command_list):
+def interpret(command_list): #interprets the arguments in the comamnd line
     try:
         if len(command_list) == 0:
-            print('Invalid arguments. Please use -t, -b, -c , -n, or -h for help.')
+            print('Invalid arguments. Please use -t, -b, -c , -n, or -h for help.') # if there are no arguments passed the program quits
             quit()
         elif command_list[0] == '-t' and command_list[1] == '-s':
             dont_sleep_for_x_time('-s', command_list[2])
@@ -102,9 +105,8 @@ def interpret(command_list):
             help_page()
         else:
             print('Invalid arguments. Please use -t, -b, -c, -i, -n, or -h for further help.')
-    except IndexError:
+    except IndexError: # if there is a wrong number of arguments passed 
         print('missing or invalid arguments.(Index error detected)')
 
-
-interpret(get_command())
-
+if __name__ == "__main__":
+    interpret(get_command()) # main 
